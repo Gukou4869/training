1c0c281e07aea5601ebe8e926a9a9bab6be8de7e
Shota Taniguchi <staniguchi@genvidtech.com>
2023-07-02 22:05:44 +0900

    set up firebase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import styles2 from "./test.module.scss";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
+        <Link href={"/login"}>Login Page!!!</Link>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
