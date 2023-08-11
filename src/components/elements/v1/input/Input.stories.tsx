0bd85771bcd78d6d713e334270233969ef052f60
Shota Taniguchi <staniguchi@genvidtech.com>
2023-08-11 19:53:44 +0900

    updated input component: added new props

--- src/components/elements/v1/input/Input.stories.tsx
+++ src/components/elements/v1/input/Input.stories.tsx
@@ -1,14 +1,16 @@
-import { StoryFn, Meta } from '@storybook/react';
-  import Input, { InputProps } from '.';
-  
-  export default {
-    title: 'Components/Input',
-    component: Input,
-  } as Meta;
-  
-  const Template: StoryFn<InputProps> = (args:InputProps) => <Input {...args} />;
-  
-  export const Default = Template.bind({});
-  Default.args = {};
-  Default.storyName = "Input"
-  
+import { StoryFn, Meta } from "@storybook/react";
+import Input, { InputProps } from ".";
+
+export default {
+  title: "Components/Input",
+  component: Input,
+} as Meta;
+
+const Template: StoryFn<InputProps> = (args: InputProps) => <Input {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  label: "password",
+  type: "password",
+};
+Default.storyName = "Input";
--- src/components/elements/v1/input/index.tsx
+++ src/components/elements/v1/input/index.tsx
@@ -1,16 +1,64 @@
-import React, { FC } from "react";
+import React, { CSSProperties, FC, useState } from "react";
 import styles from "./Input.module.scss";
+import cx from "classnames";
 
 export interface InputProps {
+  label: String;
+  type?: "text" | "email" | "password" | "tel";
+  width?: CSSProperties["width"];
+  height?: CSSProperties["height"];
   value: string;
   placeholder?: string;
+  errorMassage?: string;
+  supportText?: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 const Input: FC<InputProps> = (props) => {
-  const { value, placeholder = "ここにテキストを入力してください", onChange } = props;
+  const [isFocused, setIsFocused] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
-  return <input type="text" className={styles.input} placeholder={placeholder} value={value} onChange={onChange} />;
+  const {
+    label,
+    type = "text",
+    value,
+    placeholder = "テキストを入力してください",
+    onChange,
+    width = "18.4375rem",
+    height = "2em",
+    supportText,
+    errorMassage,
+  } = props;
+
+  return (
+    <div className={styles.inputContainer}>
+      <label htmlFor="input">{label}</label>
+      {supportText && <span>{supportText}</span>}
+      <input
+        id="input"
+        style={{ width, height }}
+        type={showPassword ? "text" : type}
+        className={cx(styles.input, errorMassage && styles.error)}
+        placeholder={isFocused ? "入力中..." : placeholder}
+        value={value}
+        onChange={onChange}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+      />
+      {type === "password" && (
+        <button
+          type="button"
+          className={styles.passwordButton}
+          onClick={() => {
+            setShowPassword((prev) => !prev);
+          }}
+        >
+          {showPassword ? "🐵" : "🙈"}
+        </button>
+      )}
+      {errorMassage && <span className={styles.error}>{errorMassage}</span>}
+    </div>
+  );
 };
 
 export default Input;
